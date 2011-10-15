d7e403869ae288d80573693ec90b4fc1a1f08575
Tjerk Santegoeds <tsan@tdias.com>
2011-10-15 21:49:09 +0100

    A bit more chicken

--- scm/my-grep/main.scm
+++ scm/my-grep/main.scm
@@ -1,15 +1,19 @@
 #!/usr/bin/env csi -ss
 
+;; Grep a single file
+(define (grep-file file)
+  (display (read-lines file))
+  (newline))
+
+
+;; Main Entry point
 (define (main)
-  (newline)
   (for-each grep-file (command-line-arguments))
   (exit))
 
-;; Grep a single file
-(define (grep-file file)
-  (display file)
-  (newline))
 
+;; Apparently this is needed for compatibility between
+;; compiled Chicken and scripted execution.
 (if (not (equal? (program-name) "csi"))
   (main))
 
